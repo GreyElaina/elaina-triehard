f951df7c766c0ab15c6cbefdc844f2be44deaaf3
Elaina <GreyElaina@outlook.com>
2024-09-19 05:45:15 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup, Extension
+from setuptools import find_packages, setup, Extension
 from os import environ
+import os
+from Cython.Build import cythonize
 
 if environ.get("NO_CYTHON"):
     setup(
@@ -10,12 +12,17 @@ if environ.get("NO_CYTHON"):
     )
     exit()
 
+extensions = [
+    Extension(
+        "elaina_triehard",  # 模块名称（不包括 .pyx 后缀）
+        sources=["src/elaina_triehard/*.pyx"],  # 源文件路径
+        include_dirs=[os.path.join(os.getcwd(), "src")],  # include path
+    )
+]
+
 setup(
-    ext_modules=(
-        [
-            Extension("elaina_triehard.impl_c", ["src/elaina_triehard/impl_c.c"]),
-        ]
-    ),
-    include_package_data=True,
-    exclude_package_data={"": ["*.c"]},
+    ext_modules=cythonize(extensions),  # 使用 cythonize 编译扩展
+    include_package_data=True,  # 包含包内的数据
+    #packages=find_packages("src"),  # 查找包
+    #exclude_package_data={"": ["*.c"]},  # 排除 .c 文件
 )
